2d42ad59294bf3ed18d7e4705757510a5e1cfbf1
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2022-10-22 00:20:54 +0300

    new inputs added

--- lib/main.dart
+++ lib/main.dart
@@ -10,52 +10,67 @@ void main() {
 
 class RentVanApp extends StatelessWidget {
   const RentVanApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rent Van App',
       theme: ThemeData(
-          scaffoldBackgroundColor: const Color.fromARGB(255, 40, 40, 40),
-          fontFamily: 'Arapey',
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color.fromARGB(255, 40, 40, 40),
-          ),
-          elevatedButtonTheme: const ElevatedButtonThemeData(
-            style: ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(
-                Color.fromARGB(255, 167, 117, 77),
-              ),
-              textStyle: MaterialStatePropertyAll(
-                TextStyle(fontSize: 15, fontFamily: 'Arapey'),
-              ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 40, 40, 40),
+        fontFamily: 'Arapey',
+        primaryColor: const Color.fromARGB(255, 40, 40, 40),
+        colorScheme: const ColorScheme(
+          background: Color.fromARGB(255, 40, 40, 40),
+          brightness: Brightness.light,
+          error: Colors.red,
+          onBackground: Color.fromARGB(255, 40, 40, 40),
+          onError: Colors.red,
+          onPrimary: Color.fromARGB(255, 244, 243, 243),
+          onSecondary: Color.fromARGB(255, 167, 117, 77),
+          onSurface: Color.fromARGB(255, 0, 0, 0),
+          primary: Color.fromARGB(255, 167, 117, 77),
+          secondary: Color.fromARGB(255, 167, 117, 77),
+          surface: Color.fromARGB(255, 0, 0, 0),
+        ),
+        highlightColor: const Color.fromARGB(255, 167, 117, 77),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 40, 40, 40),
+        ),
+        elevatedButtonTheme: const ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(
+              Color.fromARGB(255, 167, 117, 77),
+            ),
+            textStyle: MaterialStatePropertyAll(
+              TextStyle(fontSize: 15, fontFamily: 'Arapey'),
             ),
           ),
-          textTheme: const TextTheme(
-            headlineMedium: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
+        ),
+        textTheme:  const TextTheme(
+          headlineMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
           ),
-          inputDecorationTheme: const InputDecorationTheme(
-            filled: true,
-            fillColor: Color.fromARGB(255, 218, 218, 218),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(
-                  color: Color.fromARGB(255, 167, 117, 77), width: 7.0),
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-            ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Color.fromARGB(255, 218, 218, 218),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+                color: Color.fromARGB(255, 167, 117, 77), width: 7.0),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
           ),
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: Color.fromARGB(255, 167, 117, 77),
-          ),
-          primaryColor: const Color.fromARGB(255, 40, 40, 40),
-          highlightColor: const Color.fromARGB(255, 167, 117, 77)),
+        ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color.fromARGB(255, 167, 117, 77),
+        ),
+        
+      ),
       initialRoute: "/registerDriver",
       routes: {
         '/': (context) => const LoginScreen(),
         '/registerCustomer': (context) => const RegisterCustomerScreen(),
-        '/registerDriver': (context) => const RegisterDriverScreen(),
+        '/registerDriver': (context) =>  RegisterDriverScreen(),
         '/forgotPassword': (context) => const ForgotPasswordScreen()
       },
     );
--- lib/views/loginRegister/register_driver.dart
+++ lib/views/loginRegister/register_driver.dart
@@ -1,8 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rentcarmobile/constants/assets_path.dart';
 
 class RegisterDriverScreen extends StatefulWidget {
-  const RegisterDriverScreen({super.key});
+  RegisterDriverScreen({super.key});
+  String? locationDropdown = "Adana";
+  String? genderDropdown = "Male";
+  final List<String> cities = [
+    "Adana",
+    "Adiyaman",
+    "Afyon",
+    "Agri",
+    "Aksaray",
+    "Amasya",
+    "Ankara",
+    "Antalya",
+    "Ardahan",
+    "Artvin",
+    "Aydin",
+    "Balikesir",
+    "Bartin",
+    "Batman",
+    "Bayburt",
+    "Bilecik",
+    "Bingol",
+    "Bitlis",
+    "Bolu",
+    "Burdur",
+    "Bursa",
+    "Canakkale",
+    "Cankiri",
+    "Corum",
+    "Denizli",
+    "Diyarbakir",
+    "Duzce",
+    "Edirne",
+    "Elazig",
+    "Erzincan",
+    "Erzurum",
+    "Eskisehir",
+    "Gaziantep",
+    "Giresun",
+    "Gumushane",
+    "Hakkari",
+    "Hatay",
+    "Igdir",
+    "Isparta",
+    "Istanbul",
+    "Izmir",
+    "Kahramanmaras",
+    "Karabuk",
+    "Karaman",
+    "Kars",
+    "Kastamonu",
+    "Kayseri",
+    "Kilis",
+    "Kirikkale",
+    "Kirklareli",
+    "Kirsehir",
+    "Kocaeli",
+    "Konya",
+    "Kutahya",
+    "Malatya",
+    "Manisa",
+    "Mardin",
+    "Mersin",
+    "Mugla",
+    "Mus",
+    "Nevsehir",
+    "Nigde",
+    "Ordu",
+    "Osmaniye",
+    "Rize",
+    "Sakarya",
+    "Samsun",
+    "Sanliurfa",
+    "Siirt",
+    "Sinop",
+    "Sirnak",
+    "Sivas",
+    "Tekirdag",
+    "Tokat",
+    "Trabzon",
+    "Tunceli",
+    "Usak",
+    "Van",
+    "Yalova",
+    "Yozgat",
+    "Zonguldak"
+  ];
+  final List<String> genders = ["Male", "Female"];
 
   @override
   State<RegisterDriverScreen> createState() => _RegisterDriverScreenState();
@@ -71,20 +158,33 @@ class _RegisterDriverScreenState extends State<RegisterDriverScreen> {
                   Row(
                     children: [
                       //Name
-                      const Expanded(
+                      Expanded(
                         flex: 1,
                         child: TextField(
-                          decoration: InputDecoration(hintText: "Name"),
+                          decoration: const InputDecoration(
+                            hintText: "Name",
+                          ),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(
+                              RegExp("[a-zA-Z]"),
+                            ),
+                          ],
                         ),
                       ),
                       SizedBox(
                         width: phoneWidth * 0.04,
                       ),
                       //Surname
-                      const Expanded(
+                      Expanded(
                         flex: 1,
                         child: TextField(
-                          decoration: InputDecoration(hintText: "Surname"),
+                          decoration:
+                              const InputDecoration(hintText: "Surname"),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(
+                              RegExp("[a-zA-Z]"),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -96,31 +196,41 @@ class _RegisterDriverScreenState extends State<RegisterDriverScreen> {
                       Expanded(
                         flex: 1,
                         child: TextField(
-                          decoration: const InputDecoration(hintText: "Birth Date"),
+                          decoration: const InputDecoration(
+                            hintText: "Birth Date",
+                          ),
                           controller: birthDateController,
                           onTap: () async {
                             showDialog(
                                 context: context,
                                 barrierDismissible: false,
                                 builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    actions: [
-                                      CalendarDatePicker(
-                                        firstDate: DateTime.parse("1900-01-01"),
-                                        initialDate: DateTime.now(),
-                                        lastDate: DateTime.now(),
-                                        onDateChanged: (DateTime value) {
-                                          birthDateController.text =
-                                              "${value.year}-${value.month}-${value.day}";
-                                        },
-                                      ),
-                                      ElevatedButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: const Text("OK"),
-                                      ),
-                                    ],
+                                  return Theme(
+                                    data: Theme.of(context),
+                                    child: AlertDialog(
+                                      actions: [
+                                        CalendarDatePicker(
+                                          firstDate:
+                                              DateTime.parse("1900-01-01"),
+                                          initialDate:
+                                              birthDateController.text != ""
+                                                  ? DateTime.parse(
+                                                      birthDateController.text)
+                                                  : DateTime.now(),
+                                          lastDate: DateTime.now(),
+                                          onDateChanged: (DateTime value) {
+                                            birthDateController.text =
+                                                "${value.year}-${value.month < 10 ? "0${value.month}" : value.month}-${value.day < 10 ? "0${value.day}" : value.day}";
+                                          },
+                                        ),
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: const Text("OK"),
+                                        ),
+                                      ],
+                                    ),
                                   );
                                 });
                           },
@@ -130,10 +240,37 @@ class _RegisterDriverScreenState extends State<RegisterDriverScreen> {
                         width: phoneWidth * 0.04,
                       ),
                       //Gender
-                      const Expanded(
+                      Expanded(
                         flex: 1,
-                        child: TextField(
-                          decoration: InputDecoration(hintText: "Gender"),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 218, 218, 218),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: DropdownButton(
+                            value: widget.genderDropdown,
+                            items: widget.genders
+                                .map(
+                                  (value) => DropdownMenuItem(
+                                    value: value,
+                                    child: Container(
+                                      padding: const EdgeInsets.only(left: 12),
+                                      child: Text(value,
+                                          style: const TextStyle(fontSize: 17)),
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                widget.genderDropdown = value;
+                              });
+                            },
+                            dropdownColor:
+                                const Color.fromARGB(255, 218, 218, 218),
+                            borderRadius: BorderRadius.circular(10),
+                            isExpanded: true,
+                          ),
                         ),
                       ),
                     ],
@@ -142,24 +279,62 @@ class _RegisterDriverScreenState extends State<RegisterDriverScreen> {
                   Row(
                     children: [
                       //NationalID
-                      const Expanded(
+                      Expanded(
                         flex: 1,
                         child: TextField(
-                          decoration: InputDecoration(hintText: "NationalID"),
+                          decoration:
+                              const InputDecoration(hintText: "National ID"),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(
+                              RegExp("[0-9]"),
+                            ),
+                          ],
                         ),
                       ),
                       SizedBox(
                         width: phoneWidth * 0.04,
                       ),
                       //Location
-                      const Expanded(
+                      Expanded(
                         flex: 1,
-                        child: TextField(
-                          decoration: InputDecoration(hintText: "Location"),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 218, 218, 218),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: DropdownButton(
+                            value: widget.locationDropdown,
+                            items: widget.cities
+                                .map(
+                                  (value) => DropdownMenuItem(
+                                    value: value,
+                                    child: Container(
+                                      padding: const EdgeInsets.only(left: 12),
+                                      child: Text(value,
+                                          style: const TextStyle(fontSize: 17)),
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                widget.locationDropdown = value;
+                              });
+                            },
+                            dropdownColor:
+                                const Color.fromARGB(255, 218, 218, 218),
+                            borderRadius: BorderRadius.circular(10),
+                            isExpanded: true,
+                          ),
                         ),
                       ),
                     ],
                   ),
+                  const TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 5,
+                    decoration: InputDecoration(hintText: "Biography"),
+                  )
                 ],
               ),
             ),
