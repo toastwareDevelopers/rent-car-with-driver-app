982b6723d845b969f61149f15da5a262636a035e
Tk749 <tahaknl.00@gmail.com>
2022-10-22 17:02:45 +0300

    update mongodb url

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const customerAuthRouter = require("./routes/customerAuth");
 
 const PORT = 3000;
 const app = express();
-const DB = 'mongodb+srv://123asd:123asd@my.ipblsxg.mongodb.net/?retryWrites=true&w=majority'
+const DB = "mongodb+srv://toastware:habilsheild343*@cluster0.qtshlnw.mongodb.net/?retryWrites=true&w=majority"
+
 
 app.use(express.json());
 app.use(customerAuthRouter);
