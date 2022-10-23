21e2c3d9babfa3f1d980b646e8d133f3456c5ca8
Yusuf Alperen Dönmez <49916000+DaPhantomxx@users.noreply.github.com>
2022-10-23 22:53:02 +0300

    feature/loginscreen v1.0
    Made the initial version of the loginscreen, http request are not handled but will be

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\alper\\Documents\\GitHub\\Rent-Car-With-The-Driver-App" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\alper\\Documents\\GitHub\\Rent-Car-With-The-Driver-App"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\alper\\Documents\\GitHub\\Rent-Car-With-The-Driver-App\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\alper\\Documents\\GitHub\\Rent-Car-With-The-Driver-App"
+  "FLUTTER_TARGET=C:\\Users\\alper\\Documents\\GitHub\\Rent-Car-With-The-Driver-App\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\alper\\Documents\\GitHub\\Rent-Car-With-The-Driver-App\\.dart_tool\\package_config.json"
+)
