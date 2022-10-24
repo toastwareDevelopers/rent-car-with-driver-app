78956581333a5ceff84203f36991e2d77cb4f0d4
Tk749 <tahaknl.00@gmail.com>
2022-10-24 10:41:20 +0300

    add db.js for Mongodb connection

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const mongoose = require("mongoose");
 
 const customerAuthRouter = require("./routes/customerAuth");
+const connection = require('./db.js')
+
+connection()
 
 const PORT = 3000;
 const app = express();
-const DB = "mongodb+srv://toastware:habilsheild343*@cluster0.qtshlnw.mongodb.net/?retryWrites=true&w=majority"
 
 
 app.use(express.json());
 app.use(customerAuthRouter);
 
-
-mongoose.connect(DB).then(function () {
-    console.log("Connection Successful")
-}).catch(e => {
-    console.log(e);
-});
 
 app.listen(PORT, () => {
     console.log('connected ad port ' + PORT);
