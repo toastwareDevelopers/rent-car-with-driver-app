eee345b01db66f614be2d5fc8ceb0631a78aa363
Tk749 <96778661+Tk749@users.noreply.github.com>
2022-12-03 19:50:37 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose')
 
-module.exports = Connection
+const Connection = () => {
+    mongoose
+        .connect('mongodb+srv://toastware:habilsheild343*@cluster0.qtshlnw.mongodb.net/?retryWrites=true&w=majority')
+        .then( () => {
+            console.log('db connection succesfully')
+        })
+        .catch((err)=>{
+            console.log('DB connection error' + err)
+        });
+};
+
 module.exports = Connection
