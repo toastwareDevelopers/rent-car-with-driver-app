644442d5ccf742ee824c2ae5482cc885245320cb
Tk749 <96778661+Tk749@users.noreply.github.com>
2022-12-03 19:45:01 +0300

    Update customerAuth.js

--- routes/customerAuth.js
+++ routes/customerAuth.js
@@ -3,58 +3,18 @@ const Customer = require("../models/customer");
 const moment = require('moment');
 
 const bcryptjs = require('bcryptjs');
-<<<<<<< HEAD
-const Driver = require('../models/driver');
 
 const customerAuthRouter = express.Router();
 
-//customerAuthRouter.post('/api/signup/customer', async function (req, res) {
-//    try {
-//
-//        const { phoneNumber, email, password} = req.body;
-//
-//        const existingCustomerWithMail = await Customer.findOne({ email });
-//        const existingCustomerWithPhone = await Customer.findOne({ phoneNumber });
-//
-//        if (existingCustomerWithMail) {
-//            return res.status(400).json({ msg: "There is a person with same email already" });
-//        }
-//
-//        if (existingCustomerWithPhone) {
-//            return res.status(400).json({ msg: "There is a person with same phone number already" });
-//        }
-//
-//        res.sendStatus(200);
-//    } catch (error) {
-//        res.status(500).json({ error: error.message });
-//    }
-//
-//
-//});
-
-=======
-
-const customerAuthRouter = express.Router();
-
->>>>>>> feature/backend/customer
 customerAuthRouter.post('/api/signup/customer', async function (req, res) {
     try {
 
         const { phoneNumber, email, password, name, surname, birthDate, gender, nationalId, passportNumber } = req.body;
 
-<<<<<<< HEAD
-        const existingCustomerWithNationalId = await Customer.findOne({ nationalId });    
-        const existingCustomerWithMail = await Customer.findOne({ email });        
-        const existingCustomerWithPhone = await Customer.findOne({ phoneNumber });
-
-        //TODO Buralar daha iyi yazılacak quick fix
-
-=======
         const existingCustomerWithNationalId = await Customer.findOne({ nationalId });
         const existingCustomerWithMail = await Customer.findOne({ email });
         const existingCustomerWithPhone = await Customer.findOne({ phoneNumber });
         
->>>>>>> feature/backend/customer
         if (existingCustomerWithNationalId) {
             return res.status(400).json({ msg: "There is a person with same National Id already" });
         }
@@ -65,14 +25,8 @@ customerAuthRouter.post('/api/signup/customer', async function (req, res) {
 
         if (existingCustomerWithPhone) {
             return res.status(400).json({ msg: "There is a person with same phone number already" });
-<<<<<<< HEAD
-        }
-
-
-=======
         }   
         
->>>>>>> feature/backend/customer
         const hashedPassword = await bcryptjs.hash(password, 8);
 
         let customer = new Customer({
@@ -96,8 +50,4 @@ customerAuthRouter.post('/api/signup/customer', async function (req, res) {
     }
 });
 
-<<<<<<< HEAD
 module.exports = customerAuthRouter; 
-=======
-module.exports = customerAuthRouter; 
->>>>>>> feature/backend/customer
