73f923af09aca1a0b49a4872050418ad375458e4
Tk749 <96778661+Tk749@users.noreply.github.com>
2022-12-03 19:49:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-<<<<<<< HEAD
 // IMPORTS FROM PACKAGES
 const express = require('express');
 const mongoose = require("mongoose");
 
 // IMPORTS FROM OTHER FILES
+const loginAuthRouter = require("./routes/loginAuth");
 const driverAuthRouter = require("./routes/driverAuth");
 const customerAuthRouter = require("./routes/customerAuth");
-const loginAuthRouter = require("./routes/loginAuth");
+const customerMainRouter = require("./routes/customerMain");
+const customerEditRouter = require("./routes/customerEdit");
+const customerProfileRouter = require("./routes/customerProfile");
 
 // INIT
 const PORT = 3000;
 const app = express();
-const DB = "mongodb+srv://toastware:habilsheild343*@cluster0.qtshlnw.mongodb.net/?retryWrites=true&w=majority"
 const connection = require('./db.js')
 
-// middleware
 app.use(express.json());
-app.use(driverAuthRouter);
 app.use(customerAuthRouter);
+app.use(customerEditRouter);
+app.use(customerProfileRouter);
 app.use(loginAuthRouter);
+app.use(customerMainRouter);
 
-//connections
-mongoose.connect(DB).then(function ( ) {
-    console.log("Connection Successful")
-}).catch(e => {
-    console.log(e);
-});
+/* Connecting to the MongoDB Atlas cluster. */
+connection()
 
+/* Listening to the port 3000. */
 app.listen(PORT, () =>{
     console.log('connected ad port ' + PORT);
 });
+
+
