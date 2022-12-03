2b6d5dbede353c48a4e9c1889e3f41e0c6c52ae9
Tk749 <tahaknl.00@gmail.com>
2022-12-03 19:33:00 +0300

    update

--- db.js
+++ db.js
@@ -1,14 +1,3 @@
-const mongoose = require('mongoose')
 
-const Connection = () => {
-    mongoose
-        .connect('mongodb+srv://toastware:habilsheild343*@cluster0.qtshlnw.mongodb.net/?retryWrites=true&w=majority')
-        .then(() => {
-            console.log('db connection succesfully')
-        })
-        .catch((err) => {
-            console.log('DB connection error' + err)
-        });
-};
-
+module.exports = Connection
 module.exports = Connection
