ab884a9ac50858e5093d18027595fa474ddd2f05
Tk749 <tahaknl.00@gmail.com>
2022-12-09 11:18:52 +0300

    Update customer.js

--- models/customer.js
+++ models/customer.js
@@ -62,18 +62,66 @@ const customerSchema = mongoose.Schema({
     gender: {
         required: true,
         type: String,
-        enum: ["Female", "Male"],
+        enum: ["Female", "female", "Male", "male",],
     },
 
     nationalId: {
         required: true,
         type: String,
         trim: true,
+        validate: {
+            validator: function (value) {
+                const nationalIdFormat = /^[1-9][0-9]{10}$/;
+                if (value.match(nationalIdFormat)) {
+
+                    first = 0;
+                    for (let index = 0; index <= 8; index += 2) {
+                        first += Number(value.charAt(index));
+                    }
+                    first = first * 7;
+                    second = 0;
+
+                    for (let index = 1; index <= 7; index += 2) {
+                        second += Number(value.charAt(index));
+                    }
+
+
+
+
+                    tenth = first - second;
+
+
+                    last = 0;
+                    for (let index = 0; index < value.length - 1; index++) {
+                        last += Number(value.charAt(index));
+
+
+                    }
+
+
+                    if ((tenth % 10 == Number(value.charAt(9))) && (last % 10 == Number(value.charAt(10)))) {
+                        return true;
+                    }
+
+                }
+                return false;
+
+            },
+            message: "Please enter a valid National ID",
+        },
+
     },
 
     passportNumber: {
         type: String,
         trim: true,
+        validate: {
+            validator: function (value) {
+                const passportNumber = /^[a-zA-Z0-9_.+]*[a-zA-Z][a-zA-Z0-9_.+]*@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+                return value.match(passportNumber);
+            },
+            message: "Please enter a valid Passport number",
+        },
     },
 
     registerDate: {
