4f07eb66a7d538133c9ce645791921891c237da8
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2022-12-16 20:20:47 +0300

    driver filter fix

--- routes/customerMain.js
+++ routes/customerMain.js
@@ -7,7 +7,6 @@ const CustomerProfileRouter = express.Router();
 
 CustomerProfileRouter.post('/customer/main', async function (req, res) {
 
-
     try {
         var todayDate = new Date().toISOString()
         const {location, gender, language, hourlyPriceStart, hourlyPriceEnd, ratingStart, ratingEnd, ageStart,
@@ -15,8 +14,6 @@ CustomerProfileRouter.post('/customer/main', async function (req, res) {
 
         let drivers = await Driver.find({},
             { _id: 1, birthDate: 1, gender: 1, location: 1, languages: 1, rating: 1, hourlyPrice: 1, "carInfo.year": 1 })
-
-
 
         var flag
         var flag_l
@@ -97,30 +94,13 @@ CustomerProfileRouter.post('/customer/main', async function (req, res) {
         //         + " " + drivers[step].languages + " " + drivers[step].gender + " " + (drivers[step].location).toLowerCase())
 
         // }
-        var arr = []
-        arr[20] = undefined
-        var rand_number = []
-        for (var i = 0; i < (drivers.length); ++i) {
+        var sentDrivers = []
 
-            var temp = Math.floor(Math.random() * drivers.length)
-            if (rand_number.indexOf(temp) === -1) {
-                rand_number.push(temp);
-                arr[i] = drivers[i]._id
-            }
-            else i = i - 1;
+        for (var i = 0; i < 20 && i < drivers.length; ++i) {
+            sentDrivers.push(drivers[drivers.length * Math.random() | 0]);
         }
-
         
-            res.send(drivers[(rand_number[0])] + drivers[(rand_number[1])] +
-                drivers[(rand_number[2])] + drivers[(rand_number[3])] +
-                drivers[(rand_number[4])] + drivers[(rand_number[5])] +
-                drivers[(rand_number[6])] + drivers[(rand_number[7])] +
-                drivers[(rand_number[8])] + drivers[(rand_number[9])] +
-                drivers[(rand_number[10])] + drivers[(rand_number[11])] +
-                drivers[(rand_number[12])] + drivers[(rand_number[13])] +
-                drivers[(rand_number[14])] + drivers[(rand_number[15])] +
-                drivers[(rand_number[16])] + drivers[(rand_number[17])] +
-                drivers[(rand_number[18])] + drivers[(rand_number[19])])
+        res.send(sentDrivers)
 
     } catch (error) {
 
