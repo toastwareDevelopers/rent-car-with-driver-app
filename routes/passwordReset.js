cb9ceee36c19ef0848c7d6074ce31f930bd29c8d
Tk749 <96778661+Tk749@users.noreply.github.com>
2022-12-20 19:45:39 +0300

    fix mail send operation

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -6,6 +6,8 @@ const passwordResetRouter = express.Router();
 const bcryptjs = require('bcryptjs');
 const nodemailer = require("nodemailer");
 const jwt = require('jsonwebtoken');
+const sendMail = require('../middlewares/sendMail.js')
+
 
 passwordResetRouter.post('/api/passwordReset/mail', async function (req, res) {
 
@@ -19,7 +21,8 @@ passwordResetRouter.post('/api/passwordReset/mail', async function (req, res) {
         }
 
         if (profile) {
-            await sMail("toastwaredevelopers@gmail.com", profile)
+            var random = await Math.floor(Math.random() * 1234567890)
+            await sendMail("toastwaredevelopers@gmail.com", random, "Bilader niye unutuyon passwordu")
             res.sendStatus(200)
         }
 
@@ -73,39 +76,5 @@ passwordResetRouter.post('/api/passwordReset/key', async function (req, res) {
     }
 
 });
-
-async function sMail(tomail,profile) {
-
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: 'toastwaredevelopers.7@gmail.com', // generated ethereal user
-            pass: 'mdbwfhwbwvrlgfcx', // generated ethereal password
-        },
-    });
-
-    var random = Math.floor(Math.random() * 1234567890)
-    await profile.updateOne({"mailActivision.activisionKey":random})
-    await profile.updateOne({"mailActivision.activisionStatus":false})
-
-    
-    // send mail with defined transport object
-    let mailOptions = {
-        from: 'toastwaredevelopers.7@gmail.com', // sender address
-        to: tomail, // list of receivers
-        subject: "WELCOME BABY✔", // Subject line
-        // This would be the text of email body
-        text: "Hi! Just one more step to experience your RENTaCAR!.\n\n VALİDATİON KEY:  "  +  random
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-
-        if (err) console.log(err)
-    })
-}
 
 module.exports = passwordResetRouter; 
