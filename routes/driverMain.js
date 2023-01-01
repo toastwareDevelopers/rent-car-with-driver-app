46cb66b6c02de002d0ff2cf0cf97b82018a2bc20
S.Burak Yaşar <91782773+4o4x@users.noreply.github.com>
2023-01-01 22:17:48 +0300

    driver active trip

--- routes/driverMain.js
+++ routes/driverMain.js
@@ -6,4 +6,45 @@ const Driver = require("../models/driver");
 
 const driverMainRouter = express.Router();
 
+driverMainRouter.get('/driver/activeTrip' ,async function (req, res) {
+
+    const _id = req.query.ID;
+
+    //console.log(_id);
+
+    const model = await Driver.findById(_id);
+
+    
+
+    if(!model) return  res.status(400).json({msg: "There is not a model with this id"});
+    
+    const currentDate = Date.now();
+
+    //console.log(currentDate);
+
+    for (let index = 0; index < model.trips.length; index++) {
+        
+        const activeTrip = await Trip.findById(model.trips[index]);
+        if(activeTrip.startDate < currentDate && activeTrip.endDate > currentDate){
+            //console.log(currentDate);
+            
+            
+            const activeDriver = await Driver.findById(activeTrip.driverId);
+            respond = activeTrip.toObject();
+            respond.driverName = activeDriver.name;
+            respond.driverSurname = activeDriver.surname;
+            respond.profile_image64 = activeDriver.profile_image64;
+            respond.birthDate = activeDriver.birthDate;
+
+            return res.send(respond);
+        }
+        
+    }
+
+    return res.status(400).json({msg:"There is no active trip"});
+
+});
+
+module.exports = driverMainRouter;
+
 
