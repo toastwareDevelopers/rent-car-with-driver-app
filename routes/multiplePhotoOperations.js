fb452faf980f44b7f41cfddb929294b1eec23b06
Tk749 <tahaknl.00@gmail.com>
2023-01-01 17:31:58 +0300

    Update multiplePhotoOperations.js

--- routes/multiplePhotoOperations.js
+++ routes/multiplePhotoOperations.js
@@ -56,7 +56,59 @@ multiplePhoto.post('/api/driver/carPhotos/delete',async function(req,res){
 
 });
 
+multiplePhoto.get('/api/driver/documentPhotos',async function(req,res){
+    try {
+        const id = req.body.id;
+        const index = req.body.index;
+        let driver = await Driver.findById(id,
+            {legalPhotos: 1})
 
+            res.send(driver.legalPhotos[index])
+    } catch (error) {
+        /* Sending a 500 status code and the error message to the client. */
+        res.status(500).json({error: error.message});
+    }
+
+
+});
+
+
+multiplePhoto.post('/api/driver/documentPhotos',async function(req,res){
+    try {
+        const id = req.body.id;
+        const photo = req.body.photo;
+        let drivers = await Driver.findById(id,
+            {legalPhotos: 1})
+
+            await drivers.updateOne(
+                { $push: { legalPhotos: [photo] } }
+             )      
+            res.sendStatus(200)
+    } catch (error) {
+        /* Sending a 500 status code and the error message to the client. */
+        res.status(500).json({error: error.message});
+    }
+
+
+});
+
+multiplePhoto.post('/api/driver/documentPhotos/delete',async function(req,res){
+    try {
+        const id = req.body.id;
+        let drivers = await Driver.findById(id,
+            {legalPhotos: 1})
+
+            await drivers.updateOne(
+                { $unset: { legalPhotos : ""} }
+             )      
+            res.sendStatus(200)
+    } catch (error) {
+        /* Sending a 500 status code and the error message to the client. */
+        res.status(500).json({error: error.message});
+    }
+
+
+});
 
 /* Exporting the router object. */
 module.exports = multiplePhoto; 
