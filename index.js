1625c0b3d06b53c078b77a4c76a12f52846aa5ef
Tk749 <tahaknl.00@gmail.com>
2023-01-04 11:57:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const io = new Server(httpServer);
 const connection = require('./db.js');
 
 // middleware
-app.use(express.json());
+app.use(express.json().limit('2mb'));
 app.use(loginAuthRouter);
 app.use(customerAuthRouter);
 app.use(customerEditRouter);
