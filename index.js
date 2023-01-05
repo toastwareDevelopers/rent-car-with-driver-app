96239f43040a51114b75e8a1ec3ff70b49f7857c
Yusuf Alperen Dönmez <49916000+DaPhantomxx@users.noreply.github.com>
2023-01-05 14:28:06 +0300

    removed unnecessary line

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const io = new Server(httpServer);
 const connection = require('./db.js');
 
 // middleware
-app.use(express.json());
 app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({limit: '5mb',extended: true}));
 
